add8a66973368b608fe9ac8e44e2f0d76631be9b
Jin Kim <Limbomaster7@gmail.com>
2019-07-27 12:30:13 +0900

    Front end complete

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Logo.css'
+import Tilt from 'react-tilt'
+import brain from './brain.jpeg'
+const Logo = () => {
+
+    return (
+
+        <div className="ma4 mt0">
+            <Tilt className="Tilt" options={{ max: 55}} style= {{ height:250, width: 250}} >
+            <div className="Tilt-inner pa3"><img style={{paddingTop: '5px'}} src={brain} alt="" /></div>
+            </Tilt>
+        </div>
+        
+    )
+    
+    
+}
+
+
+export default Logo
